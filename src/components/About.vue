18a20eb3d533f0754b33db5f93d9ac2ceef9759a
Chen Yao <loxa0802@gmail.com>
2018-11-28 08:25:55 +0800

    Initial commit

---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <router-link to="/">Home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+      msg: 'Welcome to About'
+    }
+  }
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <router-link to="/About">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+      msg: 'Welcome to Home'
+    }
+  }
+}
+</script>
